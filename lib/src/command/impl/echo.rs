7573c3113a80d335fbadbe9bc2cf5d86831e5cfe
Vivian Hellyer <vivian@hellyer.dev>
2020-04-28 22:11:38 -0400

    lib/command/echo: add tests
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- lib/src/command/impl/echo.rs
+++ lib/src/command/impl/echo.rs
@@ -18,3 +18,47 @@ impl Dispatch for Echo {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Echo;
+    use crate::{
+        command::{CommandType, Dispatch, Request, Response},
+        Hop,
+    };
+    use alloc::vec::Vec;
+
+    #[test]
+    fn test_input() {
+        let hop = Hop::new();
+        let mut args = Vec::new();
+        args.push(b"hopdb".to_vec());
+
+        let req = Request::new(CommandType::Echo, Some(args.clone()));
+
+        assert_eq!(
+            Echo::dispatch(&hop, &req).unwrap().into_bytes(),
+            Response::from(args.as_slice()).into_bytes()
+        );
+
+        args.push(b"hop".to_vec());
+
+        let req = Request::new(CommandType::Echo, Some(args.clone()));
+        assert_eq!(
+            Echo::dispatch(&hop, &req).unwrap().into_bytes(),
+            Response::from(args.as_slice()).into_bytes()
+        );
+    }
+
+    #[test]
+    fn test_empty() {
+        let hop = Hop::new();
+        let req = Request::new(CommandType::Echo, None);
+
+        let args: &[Vec<_>] = &[];
+        assert_eq!(
+            Echo::dispatch(&hop, &req).unwrap().into_bytes(),
+            Response::from(args).into_bytes()
+        );
+    }
+}
